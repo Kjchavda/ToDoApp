bc7197a8ef958c16237835130a3a66f0d3d7895c
Kjchavda <kulanjaychavda@gmail.com>
2025-06-21 19:39:20 +0530

    Enabled CORS and requests from the frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth_router, tags_router, todos_router
 
@@ -8,6 +9,19 @@ from app.database import engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",       # React dev server
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # ✅ Allow these origins
+    allow_credentials=True,
+    allow_methods=["*"],              # ✅ Allow all HTTP methods
+    allow_headers=["*"],              # ✅ Allow all headers (including Authorization)
+)
 
 app.include_router(todos_router.todos_router)
 app.include_router(tags_router.tags_router)
